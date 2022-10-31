365f5080c5f3673306f54f5119115398f9431222
tahdi-islam <tshdi60@gmail.com>
2022-10-31 13:24:08 +0600

    add file system

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
+import { RouterProvider } from 'react-router-dom'
 import './App.css'
+import { router } from './Routes/routes'
 
 function App() {
 
   return (
     <div className="App">
-      <h1>hello genius car</h1>
-      <button className="btn btn-active btn-accent">Button</button>
-      <button className="btn btn-active btn-ghost">Button</button>
-      <button className="btn btn-active btn-link">Button</button>
+      <RouterProvider router={router}/>
     </div>
   )
 }
