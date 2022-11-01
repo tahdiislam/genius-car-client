b022f4e8f67e49512fcae15d7aa4c9803013ff11
tahdi-islam <tshdi60@gmail.com>
2022-11-01 10:48:14 +0600

    create context and a login method

---
+++ src/Context/AuthProvider.jsx
@@ -0,0 +1,41 @@
+import React, { createContext, useEffect, useState } from 'react';
+import {createUserWithEmailAndPassword, getAuth} from "firebase/auth"
+import app from '../Firebase/firebase.config';
+
+export const UserContext = createContext()
+
+const auth = getAuth(app)
+
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    // create user with email and password method
+    const createUserEmailPass = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    // load current user 
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser)
+            setLoading(false)
+        })
+        return () => unsubscribe()
+    },[])
+
+    const authInfo = {
+        user,
+        loading,
+        setLoading,
+        createUserEmailPass
+    }
+    return (
+        <UserContext.Provider value={authInfo}>
+            {children}
+        </UserContext.Provider>
+    );
+};
+
+export default AuthProvider;
